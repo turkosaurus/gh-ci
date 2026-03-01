cc6c06e6e841b503231b7b35dd21183d5d6350a8
turk <turk@grackle.club>
2026-02-28 17:33:17 -0700

    prettier logger

--- log.go
+++ log.go
@@ -14,32 +14,47 @@ import (
 func newFileLogger(format string) (*slog.Logger, error) {
 	rotated := false
 	maxAge := 1 * time.Second // FIXME: be realistic
+
+	// set path
 	logFile := os.Getenv("GH_CI_LOG_FILE")
 	if logFile == "" {
 		home, err := os.UserHomeDir()
 		logFile = filepath.Join(home, ".config", "gh-ci", "ci.log") // TODO: integrate with config
 		if err != nil {
-			return nil, fmt.Errorf("determine home directory: %w", err)
-		}
-		f, err := os.Stat(logFile)
-		if err != nil {
-			if !os.IsNotExist(err) {
-				return nil, fmt.Errorf("check log file: %w", err)
-			}
-		}
-		if time.Since(f.ModTime()) > maxAge {
-			err = os.Remove(logFile)
-			if err != nil {
-				return nil, fmt.Errorf("rotate log file: %w", err)
-			}
-			rotated = true
-		}
-		err = os.MkdirAll(filepath.Dir(logFile), 0o755)
-		if err != nil {
-			return nil, fmt.Errorf("create log directory: %w", err)
+			return nil, fmt.Errorf("determine log dir: %w", err)
 		}
 	}
-
+	// ensure dir
+	err := os.MkdirAll(filepath.Dir(logFile), 0o755)
+	if err != nil {
+		return nil, fmt.Errorf("create log directory: %w", err)
+	}
+	// check existing
+	info, err := os.Stat(logFile)
+	if err != nil {
+		// create if not exists
+		if os.IsNotExist(err) {
+			_, err := os.Create(logFile)
+			if err != nil {
+				return nil, fmt.Errorf("create log file: %w", err)
+			}
+			info, err = os.Stat(logFile)
+			if err != nil {
+				return nil, fmt.Errorf("stat log file after creation: %w", err)
+			}
+		} else {
+			return nil, fmt.Errorf("stat log file: %w", err)
+		}
+	}
+	// truncate if old
+	if time.Since(info.ModTime()) > maxAge {
+		err = os.Truncate(logFile, 0)
+		if err != nil {
+			return nil, fmt.Errorf("rotate log file: %w", err)
+		}
+		rotated = true
+	}
+	// open ready file
 	f, err := os.OpenFile(logFile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0o644)
 	if err != nil {
 		return nil, fmt.Errorf("open log file: %w", err)
@@ -54,26 +69,16 @@ func newFileLogger(format string) (*slog.Logger, error) {
 		level = slog.LevelWarn
 	}
 
-	log.New(slog.HandlerOptions{
-		Level: level,
-	}, f)
-	var handler slog.Handler
-	switch format {
-	case "json":
-		handler = slog.NewJSONHandler(f, &slog.HandlerOptions{Level: level})
-	case "text":
-		handler = slog.NewTextHandler(f, &slog.HandlerOptions{Level: level})
-	default:
-		return nil, fmt.Errorf("invalid log format: %s", format)
-	}
-
-	logger := slog.New(handler)
-	if rotated {
-		logger.Debug("logs rotated due to age")
+	logger, err := log.New(slog.HandlerOptions{Level: level}, f)
+	if err != nil {
+		return nil, fmt.Errorf("create logger: %w", err)
 	}
 	logger.Debug("initialized text file logger",
 		"path", logFile,
 		"level", level.String(),
 	)
+	if rotated {
+		logger.Debug("prior logs rotated due to age")
+	}
 	return logger, nil
 }
